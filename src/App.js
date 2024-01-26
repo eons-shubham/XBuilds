44a7fc4294ff568709a0029e5fa5a90f14010704
Shubham Sharma <eons.shubham@gmail.com>
2024-01-26 11:31:43 +0530

    feat: XDisplayName Complete

--- src/App.js
+++ src/App.js
@@ -1,35 +1,46 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import Flag from "./Flag";
+import React, { useState } from "react";
 
 function App() {
-  const container = {
-    display: "flex",
-    flexWrap: "wrap",
-    height: "100vh",
-    justifyContent: "center",
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [showName, setShowName] = useState(false);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    setShowName(true);
   };
 
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const APICall = async () => {
-      try {
-        const res = await axios.get("https://restcountries.com/v3.1/all");
-        setData(res.data);
-      } catch (error) {
-        console.log("Error fetching data: ", error);
-      }
-    };
-
-    APICall();
-  }, []);
-
   return (
-    <div style={container}>
-      {data.map((value, index) => {
-        return <Flag name={value.name.common} imageURL={value.flags.png} key={value.cca3}/>;
-      })}
+    <div>
+      <h1>Full Name Display</h1>
+      <form onSubmit={handleClick}>
+        <label htmlFor="firstname">First Name:</label>
+        <input
+          id="firstname"
+          value={firstname}
+          onChange={(e) => {
+            setFirstname(e.target.value);
+          }}
+          required={true}
+        />
+        <br />
+        <label htmlFor="lastname">Last Name:</label>
+        <input
+          id="lastname"
+          value={lastname}
+          onChange={(e) => {
+            setLastname(e.target.value);
+          }}
+          required={true}
+        />
+        <br />
+        <button onClick={handleClick}>Submit</button>
+        {showName && (
+          <p>
+            Full Name: {firstname} {lastname}
+          </p>
+        )}
+      </form>
     </div>
   );
 }
