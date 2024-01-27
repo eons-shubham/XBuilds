55e26056f24ba859fb76ddaec8e5efdd2b7ee1a0
Shubham Sharma <eons.shubham@gmail.com>
2024-01-28 00:18:25 +0530

    feat: Complete XClassComp

--- src/App.js
+++ src/App.js
@@ -1,78 +1,28 @@
-import axios from "axios";
-import { useEffect, useState, useRef } from "react";
-import Flag from "./Flag";
+import React, { Component } from "react";
 
-function App() {
-  const contBody = {
-    display: "flex",
-    flexWrap: "wrap",
-    height: "100vh",
-    justifyContent: "center",
-  };
-
-  const overall = {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  };
-
-  const [data, setData] = useState([]);
-  const [keyword, setKeyword] = useState("");
-  const timerId = useRef(null);
-  const cacheData = useRef([]);
-
-  useEffect(() => {
-    const APICall = async () => {
-      try {
-        const res = await axios.get("https://restcountries.com/v3.1/all");
-        setData(res.data);
-        cacheData.current = res.data;
-      } catch (error) {
-        console.log("Error fetching data: ", error);
-      }
-    };
-
-    APICall();
-  }, []);
-
-  const handleChange = (e) => {
-    setKeyword(e.target.value);
-  };
-
-  useEffect(() => {
-    let filterData = cacheData.current.filter((item, idx, data) => {
-      return item.name.common.toLowerCase().includes(keyword.toLowerCase());
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { counter: 0 };
+  }
+  inc = () => {
+    this.setState((state) => {
+      return { counter: state.counter + 1 };
     });
-    setData(filterData);
-  }, [keyword]);
-
-  return (
-    <div style={overall}>
-      <input
-        type="text"
-        value={keyword}
-        style={{
-          width: "50vw",
-          height: "1.5rem",
-          padding: "5px",
-          borderRadius: "5px",
-        }}
-        placeholder="Search for countries"
-        onChange={handleChange}
-      />
-      <div style={contBody}>
-        {data.map((value, index) => {
-          return (
-            <Flag
-              name={value.name.common}
-              imageURL={value.flags.png}
-              key={value.cca3}
-            />
-          );
-        })}
-      </div>
-    </div>
-  );
+  };
+  dec = () => {
+    this.setState((state) => {
+      return { counter: state.counter - 1 };
+    });
+  };
+  render() {
+    return (
+      <>
+        <h1>Counter App</h1>
+        <p>Count: {this.state.counter}</p>
+        <button onClick={this.inc}>Increment</button>
+        <button onClick={this.dec}>Decrement</button>
+      </>
+    );
+  }
 }
-
-export default App;
