040656b4f56fb46972e0553f82095ff0afbe4c5a
Shubham Sharma <eons.shubham@gmail.com>
2024-01-27 12:30:35 +0530

    feat: XStates Complete

--- src/App.js
+++ src/App.js
@@ -1,35 +1,148 @@
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useEffect, useState } from "react";
-import Flag from "./Flag";
 
 function App() {
   const container = {
     display: "flex",
-    flexWrap: "wrap",
-    height: "100vh",
     justifyContent: "center",
   };
+  const overall = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  };
+  const input = {
+    height: "30px",
+    width: "auto",
+    margin: "0.5rem",
+    borderRadius: "2px",
+  };
+  const result = {
+    marginTop: "24px",
+    fontSize: "18px",
+  };
+  const highlight = {
+    fontWeight: "bold",
+    fontSize: "24px",
+  };
 
-  const [data, setData] = useState([]);
+  const fade = {
+    color: "#888",
+  };
+
+  const [country, setCountry] = useState([]);
+  const [currentCountry, setCurrentCountry] = useState("");
+
+  const [state, setState] = useState([]);
+  const [currentState, setCurrentState] = useState("");
+
+  const [city, setCity] = useState([]);
+  const [currentCity, setCurrentCity] = useState("");
 
   useEffect(() => {
-    const APICall = async () => {
+    async function APICall() {
       try {
-        const res = await axios.get("https://restcountries.com/v3.1/all");
-        setData(res.data);
+        const res = await axios.get(
+          "https://crio-location-selector.onrender.com/countries"
+        );
+        setCountry(res.data);
       } catch (error) {
-        console.log("Error fetching data: ", error);
+        console.log("Error", error);
       }
-    };
-
+    }
     APICall();
   }, []);
 
+  useEffect(() => {
+    async function APICall() {
+      try {
+        const res = await axios.get(
+          `https://crio-location-selector.onrender.com/country=${currentCountry}/states`
+        );
+        setState(res.data);
+        setCurrentState("");
+        setCurrentCity("");
+        setCity([]);
+      } catch (error) {
+        console.log("Error", error);
+      }
+    }
+
+    if (currentCountry !== "") {
+      APICall();
+    }
+  }, [currentCountry]);
+
+  useEffect(() => {
+    async function APICall() {
+      try {
+        const res = await axios.get(
+          ` https://crio-location-selector.onrender.com/country=${currentCountry}/state=${currentState}/cities`
+        );
+        setCity(res.data);
+        setCurrentCity("");
+      } catch (error) {
+        console.log("Error", error);
+      }
+    }
+    if (currentCountry && currentState) {
+      APICall();
+    }
+  }, [currentCountry, currentState]);
+
   return (
-    <div style={container}>
-      {data.map((value, index) => {
-        return <Flag name={value.name.common} imageURL={value.flags.png} key={value.cca3}/>;
-      })}
+    <div style={overall}>
+      <h1>Select Location</h1>
+      <div style={container}>
+        <select
+          style={input}
+          value={currentCountry}
+          onChange={(e) => setCurrentCountry(e.target.value)}
+        >
+          <option disabled={true} value="">
+            Select Country
+          </option>
+          {country.map((item, idx, country) => {
+            return <option value={item}>{item}</option>;
+          })}
+        </select>
+
+        <select
+          style={input}
+          value={currentState}
+          onChange={(e) => setCurrentState(e.target.value)}
+        >
+          <option disabled={true} value="">
+            Select State
+          </option>
+          {state.map((item, idx, state) => {
+            return <option value={item}>{item}</option>;
+          })}
+        </select>
+
+        <select
+          style={input}
+          value={currentCity}
+          onChange={(e) => setCurrentCity(e.target.value)}
+        >
+          <option disabled={true} value="">
+            Select City
+          </option>
+          {city.map((item, idx, city) => {
+            return <option value={item}>{item}</option>;
+          })}
+        </select>
+      </div>
+      {currentCity && (
+        <h2 style={result}>
+          You selected <span style={highlight}>{currentCity},</span>
+          <span style={fade}>
+            {" "}
+            {currentState}, {currentCountry}
+          </span>
+        </h2>
+      )}
     </div>
   );
 }
