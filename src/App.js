d5d24ea2e8935feb9cca34383c054369ba1e0e63
Shubham Sharma <eons.shubham@gmail.com>
2024-01-29 21:37:57 +0530

    feat: Complete XPagination

--- src/App.js
+++ src/App.js
@@ -1,28 +1,118 @@
-import React, { Component } from "react";
+import React, { useEffect, useState, useRef } from "react";
+import axios from "axios";
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { counter: 0 };
-  }
-  inc = () => {
-    this.setState((state) => {
-      return { counter: state.counter + 1 };
-    });
+function App() {
+  const container = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    fontFamily: "arial, sans-serif",
   };
-  dec = () => {
-    this.setState((state) => {
-      return { counter: state.counter - 1 };
-    });
+
+  const listElement = {
+    width: "100%",
+    backgroundColor: "#009879",
+    border: "none",
+    borderCollapse: "collapse",
   };
-  render() {
-    return (
-      <>
-        <h1>Counter App</h1>
-        <p>Count: {this.state.counter}</p>
-        <button onClick={this.inc}>Increment</button>
-        <button onClick={this.dec}>Decrement</button>
-      </>
-    );
-  }
+
+  const heading = {
+    textAlign: "left",
+    color: "white",
+  };
+
+  const pageSection = {
+    backgroundColor: "#009879",
+    padding: "10px",
+    margin: "10px",
+    borderRadius: "5px",
+    color: "white",
+  };
+
+  const buttonCont = {
+    backgroundColor: "#009879",
+    padding: "10px",
+    margin: "10px",
+    borderRadius: "5px",
+    color: "white",
+    border: "none",
+  };
+
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
+  const maxlimit = useRef();
+
+  useEffect(() => {
+    async function API() {
+      try {
+        const res = await axios.get(
+          "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
+        );
+        setData(res.data);
+        maxlimit.current = Math.ceil(res.data.length / 10);
+      } catch (err) {
+        console.log("Error", err);
+        window.alert("failed to fetch data");
+      }
+    }
+    API();
+  }, []);
+
+  return (
+    <div style={container}>
+      <h1>Employee Data Table</h1>
+      <table style={listElement}>
+        <tr style={heading}>
+          <th style={{ padding: "10px" }}>ID</th>
+          <th style={{ padding: "10px" }}>Name</th>
+          <th style={{ padding: "10px" }}>Email</th>
+          <th style={{ padding: "10px" }}>Role</th>
+        </tr>
+        <tbody>
+          {data.slice((page - 1) * 10, page * 10).map((item, index, data) => {
+            return (
+              <tr
+                style={{
+                  backgroundColor: "white",
+                  border: "none",
+                }}
+              >
+                <td style={{ padding: "10px" }}>{item.id}</td>
+                <td style={{ padding: "10px" }}>{item.name}</td>
+                <td style={{ padding: "10px" }}>{item.email}</td>
+                <td style={{ padding: "10px" }}>{item.role}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+        <div style={{ height: "3px", backgroundColor: "#009879" }} />
+      </table>
+      <div>
+        <button
+          onClick={() =>
+            setPage((prev) => {
+              return Math.max(1, prev - 1);
+            })
+          }
+          style={buttonCont}
+        >
+          Previous
+        </button>
+        <span style={pageSection}>{page}</span>
+        <button
+          onClick={() =>
+            setPage((prev) => {
+              const nextPage = prev + 1;
+              return nextPage <= maxlimit.current ? nextPage : prev;
+            })
+          }
+          style={buttonCont}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
 }
+
+export default App;
